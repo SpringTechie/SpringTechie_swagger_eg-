a41cd57ac79d30d3dfd89eef43f2c9825470f00e
SpringTechie <153965307+SpringTechie@users.noreply.github.com>
2024-01-04 20:15:12 +0530

    Add files via upload

---
+++ Swagger_example/src/main/java/com/springtechie/Demo_Swagger.java
@@ -0,0 +1,17 @@
+package com.springtechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+
+public class Demo_Swagger {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo_Swagger.class, args);
+		
+	}
+	
+
+}
